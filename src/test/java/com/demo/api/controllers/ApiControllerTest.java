3c8eb0bad646cc96cb4478cf6ccb9456357b7b0f
Elias Wigsten <how2mx@gmail.com>
2020-11-19 10:58:52 +0100

    refactor(buildtool): Maven -> gradle

---
+++ src/test/java/com/demo/api/controllers/ApiControllerTest.java
@@ -0,0 +1,20 @@
+package com.demo.api.controllers;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControllerTest {
+    private final ApiController apiController = new ApiController();
+
+    @Test
+    public void helloUsesGivenName() {
+        Assertions.assertThat(apiController.hello("Elias")).isEqualTo("Hello Elias!");
+    }
+
+    @Test
+    public void pingReturnPong() {
+        Assertions.assertThat(apiController.ping()).isEqualTo("pong");
+    }
+}
