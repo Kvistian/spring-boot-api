6c7fab3fe51496b23a48c72d94f316e36c9a39be
Elias Wigsten <how2mx@gmail.com>
2020-11-20 09:06:37 +0100

    test(ApiController): Test different names

--- src/test/java/com/demo/api/controllers/ApiControllerTest.java
+++ src/test/java/com/demo/api/controllers/ApiControllerTest.java
@@ -11,6 +11,7 @@ class ApiControllerTest {
     @Test
     public void helloUsesGivenName() {
         Assertions.assertThat(apiController.hello("Elias")).isEqualTo("Hello Elias!");
+        Assertions.assertThat(apiController.hello("Mattias")).isEqualTo("Hello Mattias!");
     }
 
     @Test
